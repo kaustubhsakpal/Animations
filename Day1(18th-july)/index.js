3c1153779e045c575fb3fec5761b9dc383a95f9c
kaustubh <kaustubhsakpal9@gmil.com>
2026-07-19 22:47:29 +0530

    fourth commit

--- Day1(18th-july)/index.js
+++ Day1(18th-july)/index.js
@@ -3,38 +3,33 @@ import { gsap } from "https://cdn.jsdelivr.net/npm/gsap@3.13.0/+esm";
 let count = 0;
 
 let h1 = document.querySelector(".count");
-const tl = gsap.timeline();
-function timer() {
   const counttime = setInterval(() => {
     count++;
     h1.textContent = `${count}%`;
     if (count === 100) {
       clearInterval(counttime);
+      loadinganimation();
     }
   }, 10);
-}
-tl.from(".imagecontainer .img img", {
-    scale: 0.5,
-    y: "-100%",
-    duration: 0.8
-})
-.from(".text h1 span", {
-    y: "100%",
-    opacity: 0,
-    stagger: 0.1,
-    duration: 0.6
-});
 
-// gsap.from(".box h1 span", {
-//     scale:1.5,
-//     y: "100%",
-//     opacity: 0,
-//     duration: 0.8,
-//     // repeat: -1,
-//     // yoyo:true,
-//     stagger:{
-//         each:0.1,
-//         // from:"center"
-//     },
-//     ease: "power4.out"
-// });
+  function loadinganimation(){
+      const tl = gsap.timeline();
+      tl.to(".loader h1",{
+        opacity:0,
+        duration:1.6,
+        ease:"power.out"
+        
+      }).to(".loader",{
+        yPercent:-100,
+        duration:1.2,
+        ease:"expo.out"
+      }).from(".img",{
+        scale:1.2,
+        duration:1.3,
+        ease:"expo.out"
+      },"kaush").from(".overlaptext",{
+        opacity:0,
+        duration:1.2,
+        ease:"expo.out",
+      },"kaush").from("")
+  }
