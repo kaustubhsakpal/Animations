be0cecdcb9cf92c618c1c1a3ed1867dadde2f89a
kaustubh <kaustubhsakpal9@gmail.com>
2026-07-21 22:55:41 +0530

    index.js file changes

--- Day1(18th-july)/index.js
+++ Day1(18th-july)/index.js
@@ -1,53 +1,3 @@
-// import { gsap } from "https://cdn.jsdelivr.net/npm/gsap@3.13.0/+esm";
-// import { ScrollTrigger } from "https://cdn.jsdelivr.net/npm/gsap@3.13.0/ScrollTrigger/+esm";
-// gsap.registerPlugin(ScrollTrigger) 
-// let count = 0;
-
-// let h1 = document.querySelector(".count");
-//   const counttime = setInterval(() => {
-//     count++;
-//     h1.textContent = `${count}%`;
-//     if (count === 100) {
-//       clearInterval(counttime);
-//       loadinganimation();
-//     }
-//   }, 10);
-
-
-//   function loadinganimation(){
-//       const tl = gsap.timeline();
-//       tl.to(".loader h1",{
-//         opacity:0,
-//         duration:1.6,
-//         ease:"power.out"
-//       }).to(".loader",{
-//         xPercent:-100,
-//         duration:1.2,
-//         ease:"expo.out"
-//       }).from(".img",{
-//         scale:1.2,
-//         duration:1,
-//         ease:"expo.out",
-         
-//       },"kaush").from(".overlaptext",{
-//         opacity:0,
-//         duration:1,
-//         ease:"expo.out",
-//       },"kaush")
-//   }
-
-//   gsap.to(".app",{
-//     scale:1,
-//     scrollTrigger:{
-//       trigger:".img",
-//       start:"top top",
-//       end:"bottom top", 
-//       scrub:true,
-//       markers:true
-//     }
-//   })
-
-
 import { gsap } from "https://cdn.jsdelivr.net/npm/gsap@3.13.0/+esm";
 import { ScrollTrigger } from "https://cdn.jsdelivr.net/npm/gsap@3.13.0/ScrollTrigger/+esm";
 
@@ -57,63 +7,68 @@ let count = 0;
 const h1 = document.querySelector(".count");
 
 const counttime = setInterval(() => {
+  count++;
+  h1.textContent = `${count}%`;
 
-    count++;
-    h1.textContent = count + "%";
+  if (count === 100) {
+    clearInterval(counttime);
+    loadinganimation();
+  }
+}, 10);
 
-    if (count === 100) {
+function loadinganimation() {
 
-        clearInterval(counttime);
-        loadinganimation();
+  const tl = gsap.timeline();
+
+  tl.to(".loader h1", {
+    opacity: 0,
+    duration: 0.8
+  })
+
+  .to(".loader", {
+    yPercent:-100,
+    duration: 1.2,
+    ease: "expo.out",
+
+    onComplete: () => {
+
+      ScrollTrigger.refresh();
+
+      gsap.timeline({
+        scrollTrigger: {
+          trigger: ".app",
+          start: "top top",
+          end: "+=100%",
+          scrub: true,
+          pin: true,
+          markers: true
+        }
+      })
+
+      .fromTo(".img img",
+        {
+          scale: 1.2
+        },
+        {
+          scale: 1,
+          ease: "none"
+        },0
+      )
+
+      .fromTo(".overlaptext",
+        {
+          opacity: 0,
+          y: 80
+        },
+        {
+          opacity: 1,
+          y: 0,
+          ease: "none"
+        },0
+      );
 
     }
 
-},10);
-
-function loadinganimation(){
-
-    const tl = gsap.timeline();
-
-    tl.to(".loader h1",{
-        opacity:0,
-        duration:1
-    })
-
-    .to(".loader",{
-        xPercent:-100,
-        duration:1.2,
-
-        onComplete:()=>{
-
-            ScrollTrigger.refresh();
-
-        }
-
-    })
-
-    .from(".img",{
-        scale:1.2,
-        duration:1
-    })
-
-    .from(".overlaptext",{
-        opacity:0,
-        duration:1
-    },"<");
+  });
 
 }
-
-gsap.to(".app",{
-
-    scale:1,
-
-    scrollTrigger:{
-
-        trigger:".app",
-        start:"top top",
-        scrub:true,
-        markers:true
-
-    }
-
-});
