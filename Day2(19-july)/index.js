b70d8a8b01beeea8ffb0dcd62111e50efeea9291
kaustubh <kaustubhsakpal9@gmail.com>
2026-07-21 23:18:46 +0530

    fourth commit

--- Day2(19-july)/index.js
+++ Day2(19-july)/index.js
@@ -1,1 +1,10 @@
+import { gsap } from "https://cdn.jsdelivr.net/npm/gsap@3.13.0/+esm";
 
+gsap.from("span",{
+    ypercent:30,
+    opacity:1,
+    scale:1.2,
+    duration:1.4,
+    ease:"expo.ease.",
+    stagger:true
+})
