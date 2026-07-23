74097bddf11587d3c8c782e1c9cefd97d10c6636
kaustubh <kaustubhsakpal9@gmail.com>
2026-07-23 22:41:16 +0530

    day3 2 commit

--- Day3(22th-july)/index.js
+++ Day3(22th-july)/index.js
@@ -5,12 +5,14 @@ gsap.registerPlugin(ScrollTrigger);
 
 const section2=document.querySelector(".section2");
 
-gsap.to(".section2",{
+gsap.to(".section1",{
+    x:-200,
 scrollTrigger:{
     trigger:".section2",
-    start:"top top",
-    srub:true,
-    marker:true,
+    start:"top 50%",
+    scrub:true,
+    markers:true,
+    
 },
 yoyo:true
 })
