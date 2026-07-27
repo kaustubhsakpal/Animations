b454f1c407d3f8210c0ded34085fe57fcc166429
kaustubh <kaustubhsakpal9@gmail.com>
2026-07-27 17:32:03 +0530

    27-july 3

--- Day4(25th-july)/index.js
+++ Day4(25th-july)/index.js
@@ -1,2 +1,9 @@
-import {gsap} from "";
+import {gsap} from "https://cdn.jsdelivr.net/npm/gsap@3.13.0/+esm";
 let h1 = document.querySelector("h1");
+
+gsap.to("h1",{
+    x:200,
+    duration:1.2,
+    delay:0.5,
+    ease:"power.inOut"
+});
