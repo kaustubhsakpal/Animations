78498c2cdb35ee7e78924a4373cc4f46498cea56
kaustubh <kaustubhsakpal9@gmail.com>
2026-07-29 11:57:12 +0530

    29 july commit 4

--- Day4(25th-july)/index.js
+++ Day4(25th-july)/index.js
@@ -1,12 +1,23 @@
 import {gsap} from "https://cdn.jsdelivr.net/npm/gsap@3.13.0/+esm";
-let h1 = document.querySelector("h1");
+import { ScrollTrigger } from "https://cdn.jsdelivr.net/npm/gsap@3.13.0/ScrollTrigger/+esm";
+gsap.registerPlugin(ScrollTrigger);
+gsap.from(".h12",{
+    opacity:0,
+    x:200,
+    duration:1.2,
+    delay:0.5,
+    repeat:-1,
+    ease:"expo.inOut",
+    yoyo:true
+});
 
-// gsap.from("h1",{
-//     opacity:0,
-//     x:200,
-//     duration:1.2,
-//     delay:0.5,
-//     repeat:-1,
-//     ease:"expo.inOut",
-//     yoyo:true
-// });
+gsap.to(".h11",{
+    x:-500,
+scrollTrigger:{
+    trigger:".section2",
+    start:"top 80%",
+    end:"top top",
+    scrub:true,
+    markers:true,
+}
+})
