dce08ef6483c27b3e6647416ccc7fa3f1cf1ea79
kaustubh <kaustubhsakpal9@gmail.com>
2026-07-29 12:32:20 +0530

    29 july commit 6

--- Day4(25th-july)/index.js
+++ Day4(25th-july)/index.js
@@ -1,23 +1,42 @@
-import {gsap} from "https://cdn.jsdelivr.net/npm/gsap@3.13.0/+esm";
+import { gsap } from "https://cdn.jsdelivr.net/npm/gsap@3.13.0/+esm";
 import { ScrollTrigger } from "https://cdn.jsdelivr.net/npm/gsap@3.13.0/ScrollTrigger/+esm";
 gsap.registerPlugin(ScrollTrigger);
-gsap.from(".h12",{
-    opacity:0,
-    x:200,
-    duration:1.2,
-    delay:0.5,
-    repeat:-1,
-    ease:"expo.inOut",
-    yoyo:true
+gsap.from(".h12", {
+  opacity: 0,
+  x: 200,
+  duration: 1.2,
+  delay: 0.5,
+  repeat: -1,
+  ease: "expo.inOut",
+  yoyo: true,
 });
 
-gsap.to("img",{
-    scale:0.7,
-scrollTrigger:{
-    trigger:".section2",
-    start:"top 80%",
-    end:"top top",
-    scrub:true,
-    markers:true,
-}
-})
+gsap.to(".section1", {
+  y: -80,
+  scrollTrigger: {
+    trigger: ".section1",
+    start: "top top",
+    end: "bottom top",
+    scrub: true,
+  },
+});
+
+ScrollTrigger.create({
+  trigger: ".section1",
+  start: "top top",
+  end: "+=100%",
+  pin: true,
+  pinSpacing: false,
+});
+
+gsap.to("img", {
+  y: 200,
+  scale: 1.2,
+  scrollTrigger: {
+    trigger: ".section2",
+    start: "top 80%",
+    end: "top top",
+    scrub: true,
+    markers: true,
+  },
+});
