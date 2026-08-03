b34996fc4f3db45bea293cf3c4d773202054d58b
kaustubh <kaustubhsakpal9@gmail.com>
2026-08-03 23:21:53 +0530

    3_aug_1 2

--- Day5(2-aug)/index.js
+++ Day5(2-aug)/index.js
@@ -1,1 +1,11 @@
 import { gsap } from "https://cdn.jsdelivr.net/npm/gsap@3.13.0/+esm";
+
+gsap.to(".container span",{
+    duration: 2,
+    x: 100,
+    delay: 1,
+    ease: "power1.inOut",
+    stagger:true,
+    repeat: -1,
+
+})
